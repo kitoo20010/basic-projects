36b25582636ed0ab4fcc13e60e8d9e46fbd804a3
kitoo20010 <kitoo20010@hotmail.de>
2024-12-13 11:14:24 +0100

    new code added branch3 and updated from master branch

--- src/main/java/com/example/app/App.java
+++ src/main/java/com/example/app/App.java
@@ -10,5 +10,7 @@ public class App
     {
         System.out.println( "Hello World!" );
 		 System.out.println( "Hello World2!" );
+		
+		 System.out.println( "Hello World from branch3 !" );
     }
 }
